aa37327adaf567d46882d56bfbe8b3ac34c66f6e
jxianc <jingxianchai01@gmail.com>
2022-05-29 19:17:42 -0500

    chore: setup tailwindcss

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next'
 
-interface IndexProps {}
+interface HomeProps {}
 
-const Index: NextPage<IndexProps> = ({}) => {
-  return <div>hello world</div>
+const Home: NextPage<HomeProps> = ({}) => {
+  return (
+    <div>
+      <div className="text-red-400">hello world</div>
+    </div>
+  )
 }
 
-export default Index
+export default Home
