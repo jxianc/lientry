30bb96967a0f3c7aa84bc376e51e10e88094d8c8
Jingxian <jingxianchai01@gmail.com>
2022-06-10 15:14:21 -0500

    style: card background color

--- apps/web/pages/dashboard.tsx
+++ apps/web/pages/dashboard.tsx
@@ -8,14 +8,14 @@ const Dashboard: NextPage<DashboardProps> = ({}) => {
     <MainLayout>
       <div className="mt-4 flex flex-col space-y-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mx-12">
-          <div className="p-12 border border-gray-300 rounded-[0.3rem] items-center justify-center text-center shadow-sm">
+          <div className="p-12 bg-white border border-gray-300 rounded-[0.3rem] items-center justify-center text-center shadow-sm">
             <div className="text-2xl font-semibold">11</div>
             <div>trees created</div>
             <a href="#" className="hover:underline hover:text-teal-800">
               <span>create a new tree</span>
             </a>
           </div>
-          <div className="p-12 border border-gray-300 rounded-[0.3rem] items-center justify-center text-center shadow-sm">
+          <div className="p-12 bg-white border border-gray-300 rounded-[0.3rem] items-center justify-center text-center shadow-sm">
             <div className="text-2xl font-semibold">7</div>
             <div>trees saved</div>
             <a href="#" className="hover:underline hover:text-teal-800">
