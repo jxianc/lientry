8321b935cb94288fa58a36678e75216e2a38a903
Jingxian <jingxianchai01@gmail.com>
2022-07-06 17:59:21 -0500

    style(web): add border in link card, edit copy button styles

--- apps/web/components/cards/LinkCard.tsx
+++ apps/web/components/cards/LinkCard.tsx
@@ -19,7 +19,7 @@ export const LinkCard: React.FC<LinkCardProps> = ({
     <div className="relative">
       <NextLink href={url} passHref>
         <a target="_blank" rel="noopener noreferrer">
-          <div className="h-full space-y-6 p-4 w-full ml-auto rounded-[0.3rem] bg-li-gray-100 dark:bg-li-gray-1400 hover:bg-li-gray-200 dark:hover:bg-li-gray-1300">
+          <div className="h-full space-y-6 p-4 w-full ml-auto rounded-[0.3rem] bg-li-gray-100 dark:bg-li-gray-1400 hover:bg-li-gray-200 dark:hover:bg-li-gray-1300 border border-li-gray-200 dark:border-li-gray-1300">
             <div className="flex space-x-2 items-start mb-10">
               <div className="space-y-2">
                 <h5 className="font-semibold text-base leading-tight">
@@ -46,7 +46,7 @@ export const LinkCard: React.FC<LinkCardProps> = ({
             className={cn(
               'text-li-gray-1100 border-li-gray-400 hover:border-li-green-main hover:text-li-green-main',
               'dark:text-li-gray-700 dark:border-li-gray-1100 dark:hover:border-li-green-main dark:hover:text-li-green-main',
-              'text-sm px-1 rounded-md border inline-flex items-center space-x-0.5',
+              'text-sm px-1 rounded-[0.2rem] border flex items-center text-center justify-center space-x-0.5',
             )}
           >
             <VscCopy size={14} className="hover:cursor-pointer" />
